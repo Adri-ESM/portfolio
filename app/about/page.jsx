3fdbf672a254efbaa58da888308306b2689860bf
Adri-ESM <anggivmorales@gmail.com>
2023-06-04 02:01:56 -0400

    creating portfolio

---
+++ app/about/page.jsx
@@ -0,0 +1,5 @@
+import styles from "./About.module.css";
+
+export default function AboutPage() {
+  return <div>This is the About</div>;
+}
---
+++ app/posts/page.jsx
@@ -0,0 +1,5 @@
+import styles from "./Posts.module.css";
+
+export default function Page() {
+  return <div>There are the Posts</div>;
+}
