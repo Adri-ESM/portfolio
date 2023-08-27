744f897742dea7407a7891010ac5303df0750bd0
Enrique Fermin <efermin@aaxisdigital.com>
2023-08-27 17:58:44 -0400

    deploy

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Navigation from "../src/components/navigation/Navigation";
+import Navigation from "../src/components/navigation/navigation";
 import Footer from "@/src/components/footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
