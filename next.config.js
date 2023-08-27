5130f496666e9f40cff1b90baf803f3faa514152
Adriana Evelyn Sánchez Mejias <73199952+Adri-ESM@users.noreply.github.com>
2023-08-27 18:30:57 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,51 +1,51 @@
-// // /** @type {import('next').NextConfig} */
-// // const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-// // module.exports = nextConfig
-// module.exports = {
-//   plugins: [
-//     "postcss-flexbugs-fixes",
-//     [
-//       "postcss-preset-env",
-//       {
-//         autoprefixer: { flexbox: "no-2009" },
-//         stage: 3,
-//         features: { "custom-properties": false },
-//       },
-//     ],
-//   ],
-// };
-
-
-// @ts-check
-
-/**
- * @type {import('next').NextConfig}
- **/
-const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-   */
-  output: "export",
-
-  /**
-   * Set base path. This is usually the slug of your repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/nextjs-github-pages",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
+// module.exports = nextConfig
+module.exports = {
+  plugins: [
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: { flexbox: "no-2009" },
+        stage: 3,
+        features: { "custom-properties": false },
+      },
+    ],
+  ],
 };
 
-module.exports = nextConfig;
+
+// // @ts-check
+
+// /**
+//  * @type {import('next').NextConfig}
+//  **/
+// const nextConfig = {
+//   /**
+//    * Enable static exports for the App Router.
+//    *
+//    * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+//    */
+//   output: "export",
+
+//   /**
+//    * Set base path. This is usually the slug of your repository.
+//    *
+//    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+//    */
+//   basePath: "/nextjs-github-pages",
+
+//   /**
+//    * Disable server-based image optimization. Next.js does not support
+//    * dynamic features with static exports.
+//    *
+//    * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+//    */
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// module.exports = nextConfig;
